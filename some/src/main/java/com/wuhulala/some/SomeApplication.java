8a8cc945f3b977ee0d5be30201677538b85be435
xueaohui <xueaohui_com@163.com>
2017-04-20 20:22:24 +0800

    eureka instance info

--- some/src/main/java/com/wuhulala/some/SomeApplication.java
+++ some/src/main/java/com/wuhulala/some/SomeApplication.java
@@ -1,13 +1,19 @@
 package com.wuhulala.some;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 获取eureka的实例
+ *
  * @author xueaohui
  * @version 1.0
  * @date 2017/3/21
@@ -18,6 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SomeApplication {
 
+    @Autowired
+    EurekaClient discoveryClient;
+
     @Value("${my.message}")
     private String message;
 
@@ -25,8 +34,15 @@ public class SomeApplication {
     public String getSome(){
         return message;
     }
+
+    @GetMapping("/config-instance")
+    public InstanceInfo serviceUrl(){
+        return this.discoveryClient.getNextServerFromEureka("CONFIG",false);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SomeApplication.class,args);
     }
+
 
 }
